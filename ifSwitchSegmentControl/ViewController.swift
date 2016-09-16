ce3211f288759829269681092ed628589bc685e3
Rivail Menezes <menezes.177@hotmail.com>
2016-09-16 19:55:06 -0300

    changeColor

--- ifSwitchSegmentControl/ViewController.swift
+++ ifSwitchSegmentControl/ViewController.swift
@@ -1,25 +1,50 @@
-//
-//  ViewController.swift
-//  ifSwitchSegmentControl
-//
-//  Created by Riva on 12/09/16.
-//  Copyright © 2016 Riva. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func clicaSegmento(sender: AnyObject) {
+        
+        print(sender.selectedSegmentIndex)
+        //pode usar if
+        if(sender.selectedSegmentIndex == 0)
+        {
+            self.view.backgroundColor = UIColor.redColor()
+            
+        }
+        if(sender.selectedSegmentIndex == 1)
+        {
+            self.view.backgroundColor = UIColor.greenColor()
+            
+        }
+        if(sender.selectedSegmentIndex == 2)
+        {
+            self.view.backgroundColor = UIColor.yellowColor()
+            
+        }
+        
+        //ou use switch
+        switch(sender.selectedSegmentIndex)
+        {
+        case 0: self.view.backgroundColor = UIColor.redColor();
+        break;
+        case 1: self.view.backgroundColor = UIColor.greenColor();
+        break;
+        case 2: self.view.backgroundColor = UIColor.yellowColor();
+        break;
+        default: break;
+            
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
 
 }
 
